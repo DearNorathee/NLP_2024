f900035d491b68ecc85bfdd045929c25c0816bfc
NoratheeDear <noratee5@hotmail.com>
2024-05-11 15:58:49 +0700

    create and medium test extract_audio_1file
        # time spend 5 hr
    
        # medium tested
    
        #  tested Parameters:
            # all default parameters
            # when languages is str
    
        # untested Parameters
            # file_extension as list
            # overwrite_file = False
            # one_output_per_lang = False
            # languages as list

--- NLP 06_ffmpeg/ffmpeg_04.py
+++ NLP 06_ffmpeg/ffmpeg_04.py
@@ -17,12 +17,22 @@ def extract_audio_1file(
         alarm_done_path: Union[str,bool] = False,
         overwrite_file: bool = True,
         one_output_per_lang: bool = True,
-        language: Union[List[str],None] = None,
+        languages: Union[List[str],None] = None,
+        
                     ) -> None:
-    # time spend 4.5 hr
+    # time spend 5 hr
     
     # medium tested
     
+    #  tested Parameters:
+        # all default parameters
+        # when languages is str
+    
+    # untested Parameters
+        # file_extension as list
+        # overwrite_file = False
+        # one_output_per_lang = False
+        # languages as list
     # Not Done 
     # Next right now I got a name BigBang_FR_S06E01.mp3_EN which is wrong
     
@@ -77,12 +87,26 @@ def extract_audio_1file(
     file_extension_in = [file_extension] if isinstance(file_extension, str) else list(file_extension)
     
 
+    if output_name is None:
+        output_name_in = Path(video_path).stem
+    else:
+        output_name_in = output_name
     
+    filter_lang = [languages] if isinstance(languages,str) else languages
     
+    if languages is None:
+        filter_lang_3chr = None
+    else:
+        filter_lang_3chr = []
+    
+        for language in filter_lang:
+            lang_obj =  vt.closest_language_obj(language)
+            # variant = "B" would return fre for french
+            filter_lang_3chr.append(lang_obj.to_alpha3(variant = "B"))
     
     alarm_done_path = r"H:\D_Music\Sound Effect positive-logo-opener.mp3"
     audio_index = vt.get_audio_index(video_path)
-    metadata = vt.get_metadata(video_path,"audio",language=language)
+    metadata = vt.get_metadata(video_path,"audio",language=filter_lang_3chr)
     
     if one_output_per_lang:
         metadata_filter = metadata.drop_duplicates(subset=['language'], keep='first')
@@ -90,26 +114,26 @@ def extract_audio_1file(
         metadata_filter = metadata.copy()
     
     audio_index = list(metadata_filter.index)
-    language_list = metadata_filter['language'].tolist()
+    video_lang_list = metadata_filter['language'].tolist()
 
 
     output_name_list = []
     output_path_list = []
 
     
-    for i, language_3_str in tqdm(enumerate(language_list),total=len(language_list)):
+    for i, language_3_str in tqdm(enumerate(video_lang_list),total=len(video_lang_list)):
         
         lang_obj =  Language.get(language_3_str)
         language_2_str = str(lang_obj).upper()
         lang_obj.to_alpha3()
         for j, curr_file_ext in enumerate(file_extension_in):
             
-            if curr_file_ext not in output_name:
+            if curr_file_ext not in output_name_in:
                 if "." not in curr_file_ext:
                     file_extension_in[j] = "." + curr_file_ext
                 else:
                     file_extension_in[j] = curr_file_ext
-                curr_output_name = output_name + "_" + language_2_str + file_extension_in[j]
+                curr_output_name = output_name_in + "_" + language_2_str + file_extension_in[j]
                 output_name_list.append(curr_output_name)
                 output_path = output_folder_in / curr_output_name
                 output_path_list.append(output_path)
@@ -122,22 +146,23 @@ def extract_audio_1file(
                     "-q:a", "0",
                     str(output_path)
                 ]
+                # keep command_line for debugging
                 command_line = " ".join(command)
  
                 if os.path.exists(str(output_path)):
                     if overwrite_file:
                         os.remove(str(output_path))
                     else:
-                        print("The output path is already existed. Please delete the file or set the overwrite parameter to TRUE")
+                        print("\nThe output path is already existed. Please delete the file or set the overwrite parameter to TRUE")
                         return False
                 result = subprocess.run(command, text=True, stderr=subprocess.PIPE)
                 
                 if result.returncode != 0:
-                    print(f"Error encountered: {curr_output_name}")
+                    print(f"\nError encountered: {curr_output_name}")
                     print(result.stderr)
                 
                 elif result.returncode == 0:
-                    print(f"Extract audio successfully: {curr_output_name}!!!")
+                    print(f"\nExtract audio successfully: {curr_output_name}!!!")
                     
                     if alarm_done_path:
                         playsound(alarm_done_path)
@@ -149,22 +174,21 @@ def test_extract_audio_1file():
     alarm_done_path = r"H:\D_Music\Sound Effect positive-logo-opener.mp3"
     
     output_folder01 = r"C:\Users\Heng2020\OneDrive\D_Code\Python\Python NLP\NLP 01\OutputData\extract_audio_1file\test_01_all"
-    
+    output_folder02 = r"C:\Users\Heng2020\OneDrive\D_Code\Python\Python NLP\NLP 01\OutputData\extract_audio_1file\test_02_only_French"
     output_name = "BigBang_FR_S06E01"
     
     
     
     matrix_video_path = r"G:/My Drive/G_Videos/Polyglot/The Matrix Resurrections 2021.mkv"
     extract_audio_1file(video_path = chosen_video_path,
-                        output_folder = output_folder,
+                        output_folder = output_folder01,
                         output_name = output_name,
                         alarm_done_path = alarm_done_path)
     
     extract_audio_1file(video_path = chosen_video_path,
-                        output_folder = output_folder,
-                        output_name = output_name,
+                        output_folder = output_folder02,
                         alarm_done_path = alarm_done_path,
-                        language="french"
+                        languages="french"
                         )
     
 
