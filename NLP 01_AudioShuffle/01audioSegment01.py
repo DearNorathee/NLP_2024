6068a1ea8c4acd1c7e795a535ecb1efabdeb372c
NoratheeDear <noratee5@hotmail.com>
2024-08-18 11:10:03 +0700

    -

--- NLP 01_AudioShuffle/01audioSegment01.py
+++ NLP 01_AudioShuffle/01audioSegment01.py
@@ -20,5 +20,16 @@ def test_split_1audio_by_subtitle():
     prefix_name =   "The Ark S01E01"
     vt.split_1audio_by_subtitle(video_path,srt_path,output_folder = folder_path,alarm_done_path = alarm_done_path)
 
-    
-test_split_1audio_by_subtitle()
+def split_BigBangFR_S02E01():
+    import video_toolkit as vt
+    alarm_done_path = r"H:\D_Music\Sound Effect positive-logo-opener.mp3"
+    video_path =    Path( r"H:\D_Video\BigBang French\BigBang FR Season 02\BigBang FR S02E01.mkv")
+    srt_path =      Path( r"H:\D_Video\BigBang French\BigBang FR Season 02\BigBang FR S02E01_FR.srt")
+    folder_path =   Path( r"H:\D_Music\_Learn Languages\French\BigBang FR\Season 02\S02E01")
+    prefix_name =   "BigBang FR S01E01"
+    vt.split_1audio_by_subtitle(video_path,srt_path,output_folder = folder_path,prefix_name=prefix_name)
+    print()
+
+
+split_BigBangFR_S02E01()
+# test_split_1audio_by_subtitle()
