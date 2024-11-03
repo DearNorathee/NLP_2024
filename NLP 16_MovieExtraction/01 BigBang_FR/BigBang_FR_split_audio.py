f2ca45ccd9249bfc7e89eaf69d8a41db21435dd2
NoratheeDear <noratee5@hotmail.com>
2024-11-03 10:58:52 +0700

    first round of BigBangFR split audio from season2-11

--- NLP 16_MovieExtraction/01 BigBang_FR/BigBang_FR_split_audio.py
+++ NLP 16_MovieExtraction/01 BigBang_FR/BigBang_FR_split_audio.py
@@ -3,31 +3,55 @@ from pathlib import Path
 from pydub import AudioSegment
 import subprocess
 import os_toolkit as ost
+from tqdm.auto import tqdm
+
 
 splitted_audio_folder:list[str|Path|None] = [None for _ in range(20)]
 input_audio_folder: list[str|Path|None] = [None for _ in range(20)]
 sub_folder: list[str|Path|None] = [None for _ in range(20)]
 
 
-audio_path01 = r"H:\D_Video\BigBang French\BigBang FR Season 06\Season 06 Audio\French\BigBang FR S06E01_FR.mp3"
-sub_path01 = r"H:\D_Video\BigBang French\BigBang FR Season 06\Season 06 Audio\French Subtitle\BigBang FR S06E01_FR.srt"
+# audio_path01 = r"H:\D_Video\BigBang French\BigBang FR Season 06\Season 06 Audio\French\BigBang FR S06E01_FR.mp3"
+# sub_path01 = r"H:\D_Video\BigBang French\BigBang FR Season 06\Season 06 Audio\French Subtitle\BigBang FR S06E01_FR.srt"
 
-for i in range(1,12):
-    splitted_audio_folder[i] = fr"H:\D_Video_Python\French\BigBang French\BigBang FR Season 06\Season 06 Splitted Audio"
-    input_audio_folder[i] = fr"H:\D_Video_Python\French\BigBang French\BigBang FR Season 06\Season 06 Splitted Audio"
-    sub_folder[i] = r"H:\D_Video\BigBang French\BigBang FR Season 06\Season 06 Audio\French Subtitle\BigBang FR S06E01_FR.srt"
+# season 1 has no sub for some unknown reason
+# declare many paths
+for season_int in range(2,12):
+    season_str = str(season_int).zfill(2)
+    splitted_audio_folder[season_int] = fr"H:\D_Video_Python\French\BigBang French_test1\BigBang FR Season {season_str}\Season {season_str} Splitted Audio"
+    input_audio_folder[season_int] = fr"H:\D_Video_Python\French\BigBang French\BigBang FR Season {season_str}\Season {season_str} Splitted Audio"
+    sub_folder[season_int] = fr"H:\D_Video\BigBang French\BigBang FR Season {season_str}"
+
+prefix_names: dict[tuple[int,int], str] = {}
+
+for season in range(1,12):
+    episode_names = ost.get_filename(input_audio_folder[season])
+    n_episode = len(episode_names)
+    for episode in range(1,n_episode + 1):
+        season_str = str(season).zfill(2)
+        episode_str = str(episode).zfill(2)
+        prefix_names[(season,episode)] = f"BigBang FR S{season_str}E{episode_str}"
 
 
 #vt.is_ffmpeg_installed()
 #test_segment = AudioSegment.from_file(audio_path01)
-ost.delete_files_in_folder(splitted_audio_folder[6])
-vt.split_1audio_by_subtitle(
-    video_path = audio_path01, 
-    subtitle_path = sub_path01, 
-    output_folder = splitted_audio_folder[6],
-    modify_sub=True,
-    prefix_name="BigBang FR S06E01"
-    )
+# ost.delete_files_in_folder(splitted_audio_folder[6])
+
+for season in tqdm(range(2,12), desc= f"Season "):
+    episode_names = ost.get_filename(input_audio_folder[season])
+    n_episode = len(episode_names)
+    try:
+        vt.split_audio_by_sub(
+            media_paths = input_audio_folder[season],
+            sub_paths = sub_folder[season],
+            output_folder = splitted_audio_folder[season],
+            modify_sub = True,
+            progress_bar=True
+            
+        )
+    except:
+        print(f"There's an error at season {season}")
+
 
 
 
