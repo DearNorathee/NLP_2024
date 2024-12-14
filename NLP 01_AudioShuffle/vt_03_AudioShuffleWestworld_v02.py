a9d259aeb1e3d8c64391b1ed48fe1d7dea45e755
NoratheeDear <noratee5@hotmail.com>
2024-12-14 11:36:03 +0700

    change error to specific error type

--- NLP 01_AudioShuffle/vt_03_AudioShuffleWestworld_v02.py
+++ NLP 01_AudioShuffle/vt_03_AudioShuffleWestworld_v02.py
@@ -55,7 +55,7 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 model_base = WhisperModel('base', device="cuda")
 # playsound(alarm_path)
 model_large = WhisperModel('large-v3',device='cpu')
-playsound(alarm_path)
+# playsound(alarm_path)
 
 model_distil_large = WhisperModel('distil-large-v3')
 playsound(alarm_path)
@@ -80,7 +80,7 @@ skip_inx = [4,9,16,17,29,30,52,53,55,68,72,79,84, 124, 152, 153,
             259,
             
             ]
-easy = [1,11,14,6,8,10,7,26,32, 133, 147, 148, 165,179, 181, 188,199,200, 223, 227, 228, 237, 242, 262, 263,267, 268, 270, 273, 274]
+easy = [1,11,14,6,8,10,7,26,32, 133, 147, 148, 165,179, 181, 188,199,200, 223, 227, 228, 237, 242, 262, 263,267, 268, 270, 273, 274, 288]
 
 # (2,'2-Jul-23')
 # (6,'2-Jul-23')
@@ -121,7 +121,7 @@ random.shuffle(random_inx_list)
 #%%
 ########################## run below recurrently ##################################
 # chosen_inx = random_inx_list[3]
-chosen_inx = 275
+chosen_inx = 293
 print(f"Index: {chosen_inx}")
 audio_path = os.path.join(folder_path,file_path[chosen_inx])
 speed_factor = 1
