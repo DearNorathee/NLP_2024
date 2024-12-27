f5cdca0eb0daa6db40369c56688b002ded4b81f5
NoratheeDear <noratee5@hotmail.com>
2024-12-27 14:45:16 +0700

    manually wrote create_media_info_df

--- NLP 17_MergeLanguageVideo/extract_media.py
+++ NLP 17_MergeLanguageVideo/extract_media.py
@@ -5,14 +5,19 @@ Created on Thu Dec 12 11:48:32 2024
 @author: Heng2020
 # """
 
-# Next: convert .ass to .srt (I may need to write a new function)
+# Next: create the input_df(all combinations 24 episodes in pd.df)
 
 import video_toolkit as vt
-sub_EN_ori_folder = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles"
-sub_FR_ori_folder = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\French Ori Subtitles"
+sub_EN_ori_ass_folder = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass"
+sub_FR_ori_ass_folder = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\French Ori Subtitles\ass"
 FR_video_folder = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06"
 
-vt.extract_subtitle(FR_video_folder, output_folder = sub_EN_ori_folder,languages="eng")
-vt.extract_subtitle(FR_video_folder, output_folder = sub_FR_ori_folder,languages="fre")
+sub_EN_srt_folder = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\srt_converted"
+sub_FR_srt_folder = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\French Ori Subtitles\srt_converted"
 
+# vt.extract_subtitle(FR_video_folder, output_folder = sub_EN_ori_ass_folder,languages="eng")
+# vt.extract_subtitle(FR_video_folder, output_folder = sub_FR_ori_ass_folder,languages="fre")
+
+vt.ass_to_srt(sub_EN_ori_ass_folder,output_folder=sub_EN_srt_folder)
+vt.ass_to_srt(sub_FR_ori_ass_folder,output_folder=sub_FR_srt_folder)
 # vt.ass_to_df()
--- NLP 17_MergeLanguageVideo/vt_new_funcs.py
+++ NLP 17_MergeLanguageVideo/vt_new_funcs.py
@@ -4,12 +4,13 @@ Created on Thu Dec 12 10:34:03 2024
 
 @author: Heng2020
 """
+
+
 import os_toolkit as ost
 
-
-import whisper
-from whisper.model import Whisper as whisper_model_Whisper
-from typing import Any, Dict, List
+# import whisper
+# from whisper.model import Whisper as whisper_model_Whisper
+from typing import Any, Dict, List,Union
 import pandas as pd
 
 # import whisper.model.Whisper
@@ -23,6 +24,54 @@ import dataframe_short as ds
 from beartype import beartype
 # vt.mer
 # vt.merge_media_to1video(input_video_path, input_info_df, output_folder,)
+
+def create_media_info_df(
+    input_video_folder:str|Path    
+    ,ep_seasons: list[str]| str
+    ,media_types: list[str]
+    ,input_media_folders: list[str]
+    ,input_filname_patterns: list[str]
+    ,titles: list[str]
+    ,lang_code_3chrs: list[str]
+    ,output_folder:str
+    ) -> pd.DataFrame:
+    """
+    helper function to create media_info combinations(pd.df) to feed in to merge_media_to_video
+    """
+    def _replace_with_filename(patterns:list[str],new:str, old:str = "<>") -> List[str]:
+        result: List[str] = patterns.copy()
+        for i, x in enumerate(patterns):
+            x_new = x.replace(old,new)
+            result[i] = x_new
+        return result
+
+    lengths: List[int] = [len(media_types), len(input_media_folders), len(input_filname_patterns), len(titles), len(lang_code_3chrs)]
+    if len(set(lengths)) > 1:
+        raise ValueError(f"All input lists must have the same length, but got lengths: {lengths}")
+
+    media_per_ep = len(media_types)
+    out_df = pd.DataFrame()
+
+    for i, ep_season_text in enumerate(ep_seasons):
+        input_filenames = _replace_with_filename(input_filname_patterns,ep_season_text)
+        
+        input_media_paths = []
+        for i, filename in input_filenames:
+            input_media_paths.append(input_media_folders[i] + "/" + filename)
+        
+        output_folder_rep = [output_folder]*media_per_ep
+        df_curr_ep = pd.DataFrame({
+            'media_type': media_types,
+            'title': titles,
+            'lang_code_3alpha': lang_code_3chrs,
+            'output_folder': output_folder_rep,
+            'input_media_path':input_media_paths,
+            'output_name': 'test'
+            
+        })
+        out_df = pd.concat([out_df,df_curr_ep])
+    return out_df
+
 
 def test_merge_media_to1video():
     input_video_path = r"C:\C_Video_Python\Merge Language Video\BigBang PT Season 06\BigBang PT S06E02.mkv"
@@ -43,20 +92,8 @@ def test_merge_media_to1video():
     
     vt.merge_media_to1video(input_video_path,info_df,output_folder,output_name)
     
-test_merge_media_to1video()
 
 
-def test__create_media_dict_info():
-    media_excel_path = r"C:/Users/Heng2020/OneDrive/D_Code/Python/Python NLP/NLP 02/NLP_2024/NLP 17_MergeLanguageVideo/media_info_test1.xlsm"
-    media_df1 = ds.read_excel(media_excel_path,sheet_name="1video")
-    media_df2 = ds.read_excel(media_excel_path,sheet_name="multi")
-    
-    actual01 = vt._create_media_dict_info(media_df1)
-    actual02 = vt._create_media_dict_info(media_df2)
-    print()
-
-
-    
 def test_merge_media_to_video():
     media_excel_path = r"C:/Users/Heng2020/OneDrive/D_Code/Python/Python NLP/NLP 02/NLP_2024/NLP 17_MergeLanguageVideo/media_info_test1.xlsm"
     media_df1 = ds.read_excel(media_excel_path,sheet_name="1video")
@@ -64,61 +101,79 @@ def test_merge_media_to_video():
     
     # merge_media_to_video(media_df1)
     vt.merge_media_to_video(media_df2)
+
+def test_ass_to_srt_1file():
+    srt_path01 = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass\BigBang FR S06E01_EN.ass"
+    srt_path01_Path = Path(r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass\BigBang FR S06E01_EN.ass")
+    output_folder_01 = r"C:\C_Video_Python\Merge Language Video\tests\outputs\test_ass_to_srt_1file\test_01"
+    output_folder_01_Path = Path(r"C:\C_Video_Python\Merge Language Video\tests\outputs\test_ass_to_srt_1file\test_01")
     
-# test_merge_media_to_video()
-# test_create_media_dict_info()
+    # ass_to_srt_1file(srt_path01,output_folder_01)
+    vt.ass_to_srt_1file(srt_path01_Path,output_folder_01_Path)
 
-ass_path = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass"
-ass_sub_df = vt.ass_to_df(ass_path)
-
-# ass_sub_df.dtypes
-srt_sub_df = vt.srt_to_df(r"H:\D_Video\The Ark Season 01 Portuguese\Whisper base Subtitle PT\The Ark S01E01 PT_PT.srt")
-# srt_sub_df.dtypes
-
-
-def df_to_srt(df: pd.DataFrame, output_name: str, output_folder: Union[str, Path] = "") -> None:
-    """
-    Converts a DataFrame with subtitle data into an SRT file.
-
-    Parameters
-    ----------
-    df : pd.DataFrame
-        DataFrame containing the subtitles with columns: "sentence", "start", "end".
-    output_name : str
-        The name of the output SRT file (e.g., "subtitles.srt").
-    output_folder : str or Path, optional
-        The folder where the SRT file will be saved. Defaults to the current directory.
-
-    Returns
-    -------
-    None
-    """
-    # medium tested
+def test_ass_to_srt() -> None:
+    srt_paths_01 = r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass"
+    srt_paths_02_Path = Path(r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass")
     
-    # Ensure output folder is a Path object
-    output_folder = Path(output_folder)
-    output_path = output_folder / output_name
+    srt_paths_03 = [
+        r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass\BigBang FR S06E01_EN.ass"
+        ,r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass\BigBang FR S06E10_EN.ass"
+        ,Path(r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass\BigBang FR S06E10_EN.ass")
+        ,Path(r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\ass\BigBang FR S06E23_EN.ass")
+        ]
     
-    df_in = df.copy()
-    # Write the SRT file
-    with open(output_path, 'w', encoding='utf-8') as f:
-        df_in['start'] = df_in['start'].astype(str)
-        df_in['end'] = df_in['end'].astype(str)
-        for index, row in df_in.iterrows():
-            # Write subtitle index
-            f.write(f"{index + 1}\n")
+    output_folder_01 = r"C:\C_Video_Python\Merge Language Video\tests\outputs\test_ass_to_srt\test_01"
+    
+    output_folder_02 = Path(r"C:\C_Video_Python\Merge Language Video\tests\outputs\test_ass_to_srt\test_02_Path")
+    output_folder_03 = r"C:\C_Video_Python\Merge Language Video\tests\outputs\test_ass_to_srt\test_03_List"
+    
+    ost.delete_files_in_folder(output_folder_01)
+    ost.delete_files_in_folder(output_folder_02)
+    ost.delete_files_in_folder(output_folder_03)
+    
+    vt.ass_to_srt(srt_paths = srt_paths_01, output_folder = output_folder_01)
+    vt.ass_to_srt(srt_paths = srt_paths_02_Path, output_folder = output_folder_02)
+    vt.ass_to_srt(srt_paths = srt_paths_03, output_folder = output_folder_03)
+    
+def test_create_media_info_df():
+    input_video_folder:str|Path = r"C:\C_Video_Python\Merge Language Video\BigBang PT Season 06"
+    ep_seasons: list[str]| str = ["S06E01","S06E02","S06E03"]
+    media_types: list[str] = ["subtitle","audio","subtitle","subtitle"]
+    input_media_folders: list[str] = [r"C:\C_Video_Python\Merge Language Video\BigBang PT Season 06",
+                                    r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\Season 06 Audio",
+                                    r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\French Ori Subtitles\srt_converted",
+                                    r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\srt_converted"
+                                      ]
+    titles: list[str] = ["Portuguese_Brazilian_whisper","French","French_whisper","English_ori"]
+    lang_code_3chrs: list[str] = ["por","fre","fre","eng"]
+    output_folder:str = r"C:\C_Video_Python\Merge Language Video\tests\outputs\test_create_media_info_df\test_01"
 
-            # Format and write time stamps
-            start_time = row['start'].replace('.', ',')[:-3]
-            end_time = row['end'].replace('.', ',')[:-3]
-            f.write(f"{start_time} --> {end_time}\n")
+    # use <> to represent the ep_seasons text
+    # 
+    input_filname_patterns: list[str] = ["BigBang PT <>.srt","BigBang FR <>_FR.mp3","BigBang FR <>_FR.srt","BigBang FR <>_EN.srt"]
+    
+    actual01 = create_media_info_df(
+        input_video_folder =  input_video_folder
+        ,ep_seasons = ep_seasons
+        ,media_types = media_types
+        ,input_media_folders = input_media_folders
+        ,input_filname_patterns = input_filname_patterns
+        ,titles = titles
+        ,lang_code_3chrs = lang_code_3chrs
+        ,output_folder = output_folder)
+    print()
 
-            # Write the subtitle sentence
-            f.write(f"{row['sentence']}\n\n")
+# it seems like subtitle(ENG) in S06E19 is slightly behind the audio(after minute 12)
+# test_ass_to_srt_1file()
+test_create_media_info_df()
+test_ass_to_srt_1file()
+test_ass_to_srt()
+# vt.ass_to_srt(srt_paths, output_folder)
 
-df_to_srt(ass_sub_df[0],"BigBang FR S06E01_EN.srt",r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\srt_converted")
 
-df_to_srt(ass_sub_df[0],"BigBang FR S06E01_EN.srt",Path(r"C:\C_Video_Python\Merge Language Video\BigBang FR Season 06\English Ori Subtitles\srt_converted"))
+
+
+
 
 
 
