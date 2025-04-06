1b1774b112c45218f8799488387dd7e10e2a56b1
DearNorathee <noratee5@hotmail.com>
2025-04-06 09:55:16 +0700

    -

--- NLP 16_MovieExtraction/07 The 100/subtitle_The_100.py
+++ NLP 16_MovieExtraction/07 The 100/subtitle_The_100.py
@@ -11,7 +11,6 @@ import stable_whisper
 import whisper
 
 
-
 input_video_path = r"H:\H_Download\Video 01\2024\The 100 - 7"
 audio_PT_path = r"C:\C_Video_Python\Portuguese\The 100\Audio Extracted\The 100 season 7\Portuguese mp3"
 audio_EN_path = r"C:\C_Video_Python\Portuguese\The 100\Audio Extracted\The 100 season 7\English mp3"
