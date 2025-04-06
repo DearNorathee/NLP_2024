68fd75939ca3b33f4d5ac8e7569572da33f7af13
DearNorathee <noratee5@hotmail.com>
2025-04-06 09:52:27 +0700

    -

--- NLP 10_ChatGPT_History/scratch01.py
+++ NLP 10_ChatGPT_History/scratch01.py
@@ -8,6 +8,6 @@ import pandas as pd
 import video_toolkit as vt
 
 fr_sub_path = r"H:\H_Video_Python\Learn French\100 questions et réponses en français A1 à C1\100 questions et réponses en français (A1 à C1)_FR.srt"
-output_folder = r"H:\H_Video_Python\Learn French\100 questions et réponses en français A1 à C1"
+output_folder = r"H:\H_Video_Python\Learn French\100 questions et réponses en français A1 à C1\French_Youtube_Subtitle_st.xlsx"
 
 vt.srt_to_Excel(srt_path = fr_sub_path, output_path = output_folder)
