fb660c13583ef5195974b714349db0e48436d6d3
NoratheeLert <noratee5@hotmail.com>
2025-04-20 11:05:04 +0700

    started changing path

--- NLP 16_MovieExtraction/09 Learn French/split_FR_Youtube_video.py
+++ NLP 16_MovieExtraction/09 Learn French/split_FR_Youtube_video.py
@@ -8,19 +8,29 @@ import video_toolkit as vt
 import shutil
 import os_toolkit as ost
 
-sub_paths = r"H:\D_Video\Learn French\100 questions et réponses en français A1 à C1\100 questions et réponses en français (A1 à C1)_FR.srt"
-media_paths = r"H:\D_Video\Learn French\100 questions et réponses en français A1 à C1\100 questions et réponses en français A1 à C1.mp4"
-output_folder = r"H:\D_Video\Learn French\100 questions et réponses en français A1 à C1\Question and Answer Part_YoutubeSub"
+# sub_paths = r"H:\D_Video\Learn French\100 questions et réponses en français A1 à C1\100 questions et réponses en français (A1 à C1)_FR.srt"
+# media_paths = r"H:\D_Video\Learn French\100 questions et réponses en français A1 à C1\100 questions et réponses en français A1 à C1.mp4"
 
-output_folder02 = r"H:\D_Video\Learn French\100 questions et réponses en français A1 à C1\Question and Answer Part_YoutubeSub\100 questions et réponses en français A1 à C1."
+# output_folder = r"C:\C_Video_Python\Learn French\Elisa"
+
+# output_folder02 = r"H:\D_Video\Learn French\100 questions et réponses en français A1 à C1\Question and Answer Part_YoutubeSub\100 questions et réponses en français A1 à C1."
+
+
+sub_paths = r"C:\C_Video\Learn French\Elisa\02_The French are rude and arrogant_FR.srt"
+media_paths = r"C:\C_Video\Learn French\Elisa\02_The French are rude and arrogant.mp4"
+
+output_folder = r"C:\C_Video_Python\Learn French\Elisa"
+
+# output_folder02 = r"H:\D_Video\Learn French\100 questions et réponses en français A1 à C1\Question and Answer Part_YoutubeSub\100 questions et réponses en français A1 à C1."
+
 
 ost.delete_files_in_folder(output_folder)
 vt.split_audio_by_sub(
     media_paths = media_paths
     , sub_paths = sub_paths
     , output_folder = output_folder
-    ,prefix_names="fr_youtube01"
-    ,create_media_folder=False
+    ,prefix_names="fr_youtube02"
+    ,create_media_folder=True
     )
 # shutil.copytree()
 
