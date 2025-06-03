a20a871d77a6c3e66954311e667084a3fe84dda9
NoratheeLert <noratee5@hotmail.com>
2025-06-03 10:27:59 +0700

    Added scratch01

--- NLP 16_MovieExtraction/02 BigBang_DE/extract_audio_BigBang.py
+++ NLP 16_MovieExtraction/02 BigBang_DE/extract_audio_BigBang.py
@@ -18,6 +18,7 @@ from typing import Dict, Literal, Union, List
 from pathlib import Path
 import os
 import shutil
+import py_string_tool as pst
 
 input_video_folders: Dict[int, Union[str,Path]] = {}
 output_audio_folders: Dict[int, Union[str,Path]] = {}
@@ -30,7 +31,11 @@ skip_season = [7,8,9,10,11]
     
 # C:\C_Video_Python\The Big Bang Theory\BigBang Theory Season 01\Season 01 Audio\German
 
-
+ost.get_filename()
+pst.get_num()
+# get_folders_name(folder_path:str|Path)
+# get_folders_fullname()
+pst.get_num()
 for season in range(1,12):
     season_str = str(season).zfill(2)
     input_video_folders[season] = fr"C:\DVDFab\StreamFab\Output\Netflix\The Big Bang Theory\S{season_str}"
@@ -42,13 +47,13 @@ for season, audio_output_folder in output_audio_folders.items():
     # french_folder = Path(audio_output_folder) / "French"
     # english_folder = Path(audio_output_folder) / "English"
     language_folder = Path(audio_output_folder) / LANGUAGE
-    if season not in skip_season:
-        if os.path.exists(audio_output_folder):
-            vt.extract_audio(input_video_folders[season], audio_output_folder, languages=LANGUAGE)
-            print(f"Done season: {season}✅")
+    # if season not in skip_season:
+    #     if os.path.exists(audio_output_folder):
+    #         vt.extract_audio(input_video_folders[season], audio_output_folder, languages=LANGUAGE)
+    #         print(f"Done season: {season}✅")
 
-        else:
-            print(f"{audio_output_folder} does not exist.❌")
+    #     else:
+    #         print(f"{audio_output_folder} does not exist.❌")
     
     if season not in skip_season:
         if os.path.exists(audio_output_folder):
