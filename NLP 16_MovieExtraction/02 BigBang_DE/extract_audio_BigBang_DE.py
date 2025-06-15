2c96839aeb3189ac07a376656395e376afa7e170
NoratheeLert <noratee5@hotmail.com>
2025-06-15 09:41:20 +0700

    Added _DE file

--- NLP 16_MovieExtraction/02 BigBang_DE/extract_audio_BigBang_DE.py
+++ NLP 16_MovieExtraction/02 BigBang_DE/extract_audio_BigBang_DE.py
@@ -4,14 +4,21 @@ Created on Sat Aug 10 09:51:16 2024
 
 @author: Heng2020
 """
+
+
+# Next:
+    # Already extracted German audio & subtitles, I'll need to go to set-up files to setup all paths ready for merge
+    # Try season 2-11 first
+    
 # objective of this script is to extract audio from video in BigBang for all seasons
 
 #  change the structure of this script so that it would be very easy to run and change when there's a new langauge, or new series
 #  should take less than 10 min to change, currently it's still confusing and took more than 30 min
 
 
-# what if audio has multiple langauges?
 
+# what if audio has multiple langauges?
+import playsound
 # speed performance, it took about 10 min to extract German audio for bigbang 1 season(.mp3)
 import video_toolkit as vt
 import os_toolkit as ost
@@ -28,8 +35,13 @@ input_video_folders: Dict[int, Union[str,Path]] = {}
 output_audio_folders: Dict[int, Union[str,Path]] = {}
 
 output_audio_folders: dict[int, Union[str,Path]] = {}
+output_sub_folders: dict[int, Union[str,Path]] = {}
+
 
 LANGUAGE:str = "German"
+
+# for subtitles
+LANG_CODE:str = "deu"
 skip_season = []
 # assuming all season have the same folder structure:
     
@@ -46,21 +58,11 @@ for i, season in enumerate(avaliable_seasons):
     season_str = str(season).zfill(2)
     input_video_folders[season] = fodder_paths[i]
     output_audio_folders[season] = fr"C:\C_Video_Python\The Big Bang Theory\BigBang Theory Season {season_str}\Season {season_str} Audio\{LANGUAGE}"
+    output_sub_folders[season] = fr"C:\C_Video_Python\The Big Bang Theory\BigBang Theory Season {season_str}\Season {season_str} Subtitle\German Netflix"
 
-
-
+# extract audio
 for season, audio_output_folder in output_audio_folders.items():
-    # french_folder = Path(audio_output_folder) / "French"
-    # english_folder = Path(audio_output_folder) / "English"
-    # language_folder = Path(audio_output_folder) / LANGUAGE
-    # if season not in skip_season:
-    #     if os.path.exists(audio_output_folder):
-    #         vt.extract_audio(input_video_folders[season], audio_output_folder, languages=LANGUAGE)
-    #         print(f"Done season: {season}✅")
 
-    #     else:
-    #         print(f"{audio_output_folder} does not exist.❌")
-    
     if season not in skip_season:
         if os.path.exists(audio_output_folder):
             if not os.path.exists(output_audio_folders[season]):
@@ -78,3 +80,33 @@ for season, audio_output_folder in output_audio_folders.items():
 
 # vt.extract_audio(video_folder=input_video_folders[1], output_folder = output_audio_folders[1],languages="German")
 
+# English and German subtitles are extracted manually by changing LANG_CODE & output_sub_folders[season]
+# extract subtitle
+for season, audio_output_folder in output_audio_folders.items():
+    if season not in skip_season:
+        if os.path.exists(audio_output_folder):
+            if not os.path.exists(output_audio_folders[season]):
+                os.makedirs(output_audio_folders[season], exist_ok=True)
+            
+            ost.delete_files_in_folder(output_sub_folders[season],verbose=0)
+            vt.extract_subtitle(
+                video_folder = input_video_folders[season],
+                output_folder = output_sub_folders[season],
+                languages= LANG_CODE
+                )
+                
+            print(f"Done season: {season}✅")
+        else:
+            print(f"{audio_output_folder} does not exist. ❌")
+
+# clean up subtitles filenames(only for German)
+for season, audio_output_folder in output_audio_folders.items():
+    if season not in skip_season:
+        ost.auto_rename_series(folder_path = output_sub_folders[season], prefix = "BigBang DE")
+# vt.extract_subtitle(video_folder=input_video_folders[1], output_folder = output_sub_folders[1],languages= LANG_CODE)
+
+# ost.auto_rename_series(folder_path = output_sub_folders[1], prefix = "BigBang DE")
+#     )
+# vt.extract_sub_1_video(
+#     video_path = r"C:\DVDFab\StreamFab\Output\Netflix\The Big Bang Theory\S01\BigBang DE S01E01.mp4"
+#     , output_folder = output_sub_folders[1])
---
+++ NLP 17_MergeLanguageVideo/BigBang/BigBang DE/merge_BigBang_season_DE.py
@@ -0,0 +1,128 @@
+
+import os_toolkit as ost
+from pprint import pprint
+import shutil
+from tqdm import tqdm
+import video_toolkit as vt
+from pathlib import Path
+import pandas as pd
+
+# season 1 the french audio doesn't seem to syn up perfectly
+
+
+# NEXT: 
+# 1) Check what's wrong with S07E06, S07E19
+    # => it's French subtitle format issue
+# 2) Ran and found that for some season audio needs scaling in order to align French audio to Portugese
+#   making hard to automate and pushing the Energy for this project up
+
+# I found the issue now, again it seems to be the problem when convertting .ass to .srt
+# error S08E09
+    #  the error seems to come from French_ori
+    # 00:00:02:00,000 it should not have 00: in front
+#%%
+# BigBang8.01,8.17,8.19 has no audio(French)
+# Currently I'm not sure what's the problems but all of videos above have Brazilian Portuguese [Force] subtitle
+# The problem is not because the French audio itself is silient
+# what even stranger, if you try to remove all of audio from input_video and only include French, the audio would work
+# Also non of subtitles work in these videos
+# I might have to fix this manaually
+
+
+#%%
+def merge_media_info_df_1season(
+        season:int
+        ,test:int
+        ):
+    # error at S07E06
+    
+    season_str = str(season).zfill(2)
+    input_video_folder:str|Path = fr"C:\C_Video\BigBang Portuguese\BigBang PT Season {season_str}"
+    ep_seasons: list[str]| str = []
+    
+    test_folder = f"test_{str(test).zfill(2)}"
+    
+    output_folder:str = fr"C:\C_Video_Python\Merge Language Video\BigBang Merged\BigBang Season {season_str}" + "/" + test_folder
+    
+    if season == 1:
+        for i in range(1,18):
+            ep_str = str(i).zfill(2)
+            ep_seasons.append(f"S{season_str}E{ep_str}")
+    elif 2 >= season >= 3:
+        # season 2&3 have 23 ep's
+        for i in range(1,24):
+            ep_str = str(i).zfill(2)
+            ep_seasons.append(f"S{season_str}E{ep_str}")
+    elif season > 1:
+        for i in range(1,25):
+            ep_str = str(i).zfill(2)
+            ep_seasons.append(f"S{season_str}E{ep_str}")
+
+    list_of_info: list[tuple[str,str,str]] = [
+        # ('media_type','title','lang','input_filename_pattern')
+        
+        # ("subtitle", "English_ori",                 "eng",  "BigBang FR <>_1.srt"),
+        # ("subtitle", "French_ori",                  "fre",  "BigBang FR <>_1.srt"),
+        # ("audio",    "French",                      "fre",  "BigBang FR <>_FR.mp3"),
+        # ("subtitle", "French_whisper",              "fre",  "BigBang FR <>_FR.srt"),
+        
+        ("subtitle", "German_Netflix",              "deu",  "BigBang DE <>.srt"),
+        ("audio", "German_Netflix",              "deu",  "BigBang DE <>_DE.srt"),
+    ]
+
+    # media_types: list[str] = ["subtitle","subtitle","subtitle","audio","subtitle"]
+    # titles: list[str] = ["Portuguese_Brazilian_ori","English_ori","French_ori","French","French_whisper"]
+    # lang_code_3chrs: list[str] = ["por","eng","fre","eng"]
+    # input_filname_patterns: list[str] = ["BigBang PT <>.srt","BigBang FR <>_FR.mp3","BigBang FR <>_FR.srt","BigBang FR <>_EN.srt"]
+
+    input_media_folders: list[str] = [
+        fr"C:\C_Video_Python\The Big Bang Theory\BigBang Theory Season {season_str}\Season {season_str} Subtitle\German Netflix",
+        fr"C:\C_Video_Python\The Big Bang Theory\BigBang Theory Season {season_str}\Season {season_str} Audio\German",
+
+                                     ]
+    
+    
+    output_folder_Path: Path = Path(output_folder)
+    output_folder_Path.mkdir(parents=True, exist_ok=True)
+    
+    # use <> to represent the ep_seasons text
+    media_info_df_season:pd.DataFrame
+    
+    media_info_df_season = vt.create_media_info_df(
+        input_video_folder =  input_video_folder
+        ,input_video_pattern = "BigBang PT <>.mkv"
+        ,ep_seasons = ep_seasons
+        ,input_media_folders = input_media_folders
+        ,list_of_info = list_of_info
+        ,output_folder = output_folder)
+    try:
+        media_info_df_season.to_excel(f"BigBang PT Season {season_str}_media info.xlsx")
+    except PermissionError:
+        print("The excel file media info is opened. Skip saving it....")
+    # ############################## keep this to debug 1 episode #############################################
+    # media_info_df_season_1ep = media_info_df_season.loc[media_info_df_season["input_video_name"].isin(["BigBang PT S01E01.mkv"])]
+    
+    ost.delete_files_in_folder(output_folder)
+    
+    # vt.merge_media_to1video(
+    #     input_video_path= media_info_df_season_1ep.loc[0,"input_video_path"],
+    #     input_info_df = media_info_df_season_1ep,
+    #     output_folder = output_folder,
+    #     errors="raise"
+    #                         )
+    # ---------------------------------------- keep this to debug 1 episode -------------------------------------------------
+    
+    vt.merge_media_to_video(media_info_df_season,errors="warn")
+    print()
+
+merge_media_info_df_1season(5,1)
+# for season 2 too₭about 6 min 30 s
+
+input_video_path01 = r"H:\H_Video_Python\Merge Language Video\BigBang Merged\BigBang Season 07\BigBang PT S07E01.mkv"
+meta_data01 = vt.get_all_metadata(input_video_path01)
+
+
+# ost.auto_rename_series(folder_path = "H:\H_Video\BigBang Portugues\BigBang PT Season 05", prefix = "BigBang PT")
+# ost.rename_files_replace_text(folder_path, old_text, new_text)
+
+
